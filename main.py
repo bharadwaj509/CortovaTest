5b8ec1414259a9ee8bdceedbf25ed9f5f44c6706
bharadwaj509 <bharadwaj509@gmail.com>
2020-12-07 13:24:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ import sys
 # ExcelLoader().loadFile() method takes the file names and processes them returning an generator object.
 # The generator object will be passed to JsonExtractor().createJson() method and will be converted into json.
 # The result will stored in the same folder as the code.
-
-obj = ExcelLoader().loadFile(sys.argv[1:])
-resFile = JsonExtractor().createJson(obj)
-print("Resuls are stored in", resFile)
+if len(sys.argv) > 1:
+    obj = ExcelLoader().loadFile(sys.argv[1:])
+    resFile = JsonExtractor().createJson(obj)
+    print("Resuls are stored in", resFile)
+else:
+    print("No files are mentioned along with the command. Please follow the following syntax.")
+    print("test.py eg1.csv eg2.csv")
